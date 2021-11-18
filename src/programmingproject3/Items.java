c3839894b9ccd8b87f2d3730aee5de3604bb022f
Ramiro Mendez <eyi617@my.utsa.edu>
2021-11-17 21:36:06 -0600

    rough start

--- src/programmingproject3/Items.java
+++ src/programmingproject3/Items.java
@@ -1,4 +1,34 @@
 package programmingproject3;
 
+
 public class Items {
+    String name;
+    int price;
+    int value;
+
+    public Items(String name, int price, int value) {
+        setName(name);
+        setPrice(price);
+        setValue(value);
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+         return this.name;
+    }
+    public void setPrice(int price){
+        this.price = price;
+    }
+    public int getPrice(){
+        return this.price;
+    }
+    public void setValue(int value){
+        this.value = value;
+    }
+    public int getValue(){
+        return this.value;
+    }
+
 }
--- src/programmingproject3/Main.java
+++ src/programmingproject3/Main.java
@@ -1,9 +1,71 @@
 package programmingproject3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
-    public void main(){
+    public static void main(String [] args){
 
+        //loadItems("items.csv");
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter the no of items:");
+
+        int n = myObj.nextInt();
+        int val[] = new int[n + 1];
+        int cost[] = new int[n + 1];
+
+        System.out.println("Enter the value of items:");
+        for(int i = 0;i < n; i++)
+            val[i] = myObj.nextInt();
+        System.out.println("Enter the cost of items:");
+        for(int i = 0;i < n; i++)
+            cost[i] = myObj.nextInt();
+        int W = 100;
+        System.out.println("the max value we can get is");
+        System.out.println(KnapSack(W, cost, val, n));
 
     }
+/*
+    public static void loadItems(String fileName){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String line1;
+            br.readLine();
+            while( (line1 = br.readLine()) != null ) { //loops through a .csv file until it reaches a null line
+                String [] array = line1.split(",");
+                Items s = new Items (
+                        array[0],
+                        Integer.parseInt(array[1]),
+                        Integer.parseInt(array[2])
+                ); //creates a new store object
+                //FIXME add items to array or arraylist here
+            }
+        } catch (IOException e) { // if file is not found
+            e.printStackTrace();
+        }
+    }
+ */
+    static int max(int a,int b) {
+        return Math.max(a, b);
+    }
+
+    static int KnapSack(int W, int cost[], int val[], int n) {
+        int i, w;
+        int[][] K = new int [n+1][W+1];
+
+        for(i = 0; i < n; i++){
+            for(w = 0; w <= W; w++){
+                if(i == 0 ||w == 0)
+                    K[i][w] = 0;
+                else if(cost[i - 1] <= w)
+                    K[i][w] = max(val[i - 1] + K[i - 1][w - cost[i - 1]], K[i - 1][w]);
+                else K[i][w]=K[i - 1][w];
+            }
+        }
+        return K[n][W];
+    }
+
 }
